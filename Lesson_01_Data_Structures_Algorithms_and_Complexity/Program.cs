d258e3a2f069f26f809e93cc03352e3c7b276e88
bulhard <pravoslav.milenkov@gmail.com>
2022-02-02 16:05:12 +0200

    Add project files.

---
+++ Lesson_01_Data_Structures_Algorithms_and_Complexity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_01_Data_Structures_Algorithms_and_Complexity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ _Presentations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Presentations
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello DSA!");
+        }
+    }
+}
