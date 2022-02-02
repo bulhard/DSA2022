3be90a41ecc8955b0e9863cac6bb01ea48a9621e
bulhard <pravoslav.milenkov@gmail.com>
2022-02-02 16:06:25 +0200

    Initial commit

--- Lesson_01_Data_Structures_Algorithms_and_Complexity/Program.cs
+++ Lesson_01_Data_Structures_Algorithms_and_Complexity/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Lesson_01_Data_Structures_Algorithms_and_Complexity
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello DSA!");
         }
     }
 }
