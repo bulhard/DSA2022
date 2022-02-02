88e41260087f4b0dbfa60d9ab0d7cc19c84e6acb
bulhard <pravoslav.milenkov@gmail.com>
2022-02-02 20:13:11 +0200

    Sample 01

---
+++ Lesson_01_Data_Structures_Algorithms_and_Complexity/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_01_Data_Structures_Algorithms_and_Complexity
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public int StudentNumber { get; set; }
+    }
+}
